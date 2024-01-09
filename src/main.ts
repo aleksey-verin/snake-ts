ff8e1481da5ddf68359713d636c91f34b20f4201
Aleksey Verin <verevaa@gmail.com>
2024-01-09 13:23:19 +0300

    feat: started with snake logic

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,43 @@
 import './style.css'
 
-console.log('hey');
+// Define HTML elements
+const board = document.getElementById('game-board') as HTMLElement
+
+// Define game variables
+let snake: Array<Position> = [{ x: 10, y: 10 }]
+
+type Position = {
+  x: number
+  y: number
+}
+
+// Draw game map, snake, food
+function draw() {
+  board.innerHTML = ''
+  drawSnake()
+}
+
+// Draw snake
+function drawSnake() {
+  snake.forEach((segment) => {
+    const snakeElement = createGameElement('div', 'snake')
+    setPosition(snakeElement, segment)
+    board.appendChild(snakeElement)
+  })
+}
+
+// Create a snake or food cube/div
+function createGameElement(tag: string, className: 'snake' | 'food') {
+  const element = document.createElement(tag)
+  element.className = className
+  return element
+}
+
+// Set the position of snake or food
+function setPosition(element: HTMLElement, position: Position) {
+  element.style.gridColumn = String(position.x)
+  element.style.gridRow = String(position.y)
+}
+
+// Testing draw function
+draw();
